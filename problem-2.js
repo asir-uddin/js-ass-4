34bd369033f4e4e2bb9f51ae822081059eb3da38
Asir <mdasir017@gmaol.com>
2025-08-19 12:49:47 +0600

    first commit

---
+++ problem-2.js
@@ -0,0 +1,16 @@
+function  onlyCharacter( str ) {
+         if(typeof str !== 'string'){
+            return 'invalid';
+         }
+         let noSpace = str.replace(/\s/g, '');
+         let result = noSpace.toUpperCase();
+         return result;
+}
+   
+
+// console.log(onlyCharacter("  h e llo wor   ld" ));
+// console.log(onlyCharacter("Cy   bar- At  tac k  "));
+// console.log(onlyCharacter(" ha ck m e 1 @ru.c  n  "));
+// console.log(onlyCharacter("Serv er : : Do wn"));
+// console.log(onlyCharacter(["hack", "me"]));
+// console.log(onlyCharacter(true));
